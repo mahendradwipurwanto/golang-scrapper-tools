9d44b67d7779305ce7bb23ac5180a8405582c46a
Mahendra Dwi Purwanto <mahendradwipurwanto@gmail.com>
2023-08-18 17:44:33 +0700

    Fix version

---
+++ go.mod
@@ -0,0 +1,13 @@
+module main
+
+go 1.20
+
+require (
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mime"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/joho/godotenv"
+)
+
+type FileUploadStatus struct {
+	ID     uint   `json:"id"`
+	URL    string `json:"url"`
+	Status string `json:"status"`
+}
+
+type Files struct {
+	ID       uint
+	DriveURL string `gorm:"column:Content"`
+	Column   string `gorm:"column:nama_file"`
+}
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASS")
+	dbName := os.Getenv("DB_NAME")
+
+	tableName := os.Getenv("TABLE_NAME")
+	urlColumnName := os.Getenv("URL_COLUMN_NAME")
+	fileNameColumnName := os.Getenv("FILE_NAME_COLUMN_NAME")
+	tableWhereColumn := os.Getenv("TABLE_WHERE_COLUMN")
+	tableWhereValue := os.Getenv("TABLE_WHERE_VALUE")
+	directoryBase := os.Getenv("DIRECTORY_BASE")
+	fileNameHost := os.Getenv("FILE_NAME_HOST")
+	fileNamePrefix := os.Getenv("FILE_NAME_PREFIX")
+
+	dbConnectionStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
+
+	db, err := gorm.Open("mysql", dbConnectionStr)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	defer db.Close()
+
+	var data []Files
+	var totalData int64
+
+	// Fetch data based on the specific column condition
+	query := fmt.Sprintf("SELECT id, %s, %s FROM %s WHERE %s = '%s' AND %s != '' AND %s != NULL", urlColumnName, fileNameColumnName, tableName, tableWhereColumn, tableWhereValue, urlColumnName, urlColumnName)
+	if err := db.Raw(query).Scan(&data).Error; err != nil {
+		log.Fatal("Failed to retrieve data from database:", err)
+	}
+	if err := db.Raw(query).Count(&totalData).Error; err != nil {
+		log.Fatal("Failed to retrieve total data from database:", err)
+	}
+
+	var successfulUploads []FileUploadStatus
+	var failedUploads []FileUploadStatus
+
+	currentData := 1
+	for _, file := range data {
+		// Log the column value for debugging
+		log.Printf("File %d/%d | File ID: %d, Drive URL: %s\n", currentData, totalData, file.ID, file.DriveURL)
+		newURL, newFileName, saveSuccess := saveFileLocally(file.ID, file.DriveURL, directoryBase, file.Column, fileNamePrefix)
+
+		if saveSuccess {
+			successfulUploads = append(successfulUploads, FileUploadStatus{ID: file.ID, URL: newURL, Status: "success"})
+
+			// Update the drive_url and new column with the new values
+			db.Table(tableName).Where("id = ?", file.ID).Updates(map[string]interface{}{
+				urlColumnName:      fmt.Sprintf("%s/%s", fileNameHost, newURL),
+				fileNameColumnName: newFileName,
+			})
+		} else {
+			failedUploads = append(failedUploads, FileUploadStatus{ID: file.ID, URL: file.DriveURL, Status: "failed"})
+		}
+		currentData = currentData + 1
+	}
+
+	logResults(successfulUploads, failedUploads)
+}
+
+func saveFileLocally(fileId uint, fileURL, directoryBase, subDirectory, fileNamePrefix string) (string, string, bool) {
+	response, err := http.Get(fileURL)
+	if err != nil {
+		log.Println("Failed to download "+fileURL+" file:", err)
+		return fileURL, "", false
+	}
+	defer response.Body.Close()
+
+	fileData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println("Failed to read file data:", err)
+		return "", "", false
+	}
+
+	// Extract the file extension from the URL
+	fileExtension := filepath.Ext(fileURL)
+
+	if fileExtension == "" {
+		contentType := response.Header.Get("Content-Type")
+		fileExtensions, err := mime.ExtensionsByType(contentType)
+		if err != nil || len(fileExtensions) == 0 {
+			log.Println("Failed to determine file extension from Content-Type:", err)
+			fileExtensions = []string{".unknown"} // Default to ".unknown" extension
+		}
+
+		// Extract the extension (without the square brackets)
+		ext := fileExtensions[0]
+		fileExtension = ext
+	}
+
+	// cek if can't get extension
+	if fileExtension == "" {
+		log.Println(fmt.Sprintf("Failed to get file extension id: %v, url: %s", fileId, fileURL))
+		return "", "", false
+	}
+
+	log.Println(fileExtension)
+
+	fileIdStr := strconv.FormatUint(uint64(fileId), 10)
+	localFileName := fmt.Sprintf("%s-%s%s", fileNamePrefix, fileIdStr, fileExtension) // Set extension here
+	localFilePath := fmt.Sprintf("%s/%s/%s", directoryBase, subDirectory, localFileName)
+	localFilePath = strings.ReplaceAll(localFilePath, "//", "/")
+	// Create the subdirectories if they don't exist
+	if err := os.MkdirAll(fmt.Sprintf("%s/%s", directoryBase, subDirectory), os.ModePerm); err != nil {
+		log.Println("Failed to create subdirectories:", err)
+		return "", "", false
+	}
+
+	err = ioutil.WriteFile(localFilePath, fileData, 0644)
+	if err != nil {
+		log.Println("Failed to save file locally:", err)
+		return "", "", false
+	}
+
+	log.Printf("File %s saved successfully locally\n", fileURL)
+	return localFilePath, localFileName, true
+}
+
+func logResults(successfulUploads, failedUploads []FileUploadStatus) {
+	results := struct {
+		SuccessfulUploads []FileUploadStatus `json:"successful_uploads"`
+		FailedUploads     []FileUploadStatus `json:"failed_uploads"`
+	}{
+		SuccessfulUploads: successfulUploads,
+		FailedUploads:     failedUploads,
+	}
+
+	logFile, err := os.Create("upload_results.json")
+	if err != nil {
+		log.Println("Failed to create log file:", err)
+		return
+	}
+	defer logFile.Close()
+
+	encoder := json.NewEncoder(logFile)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(results); err != nil {
+		log.Println("Failed to encode log data:", err)
+	}
+}
